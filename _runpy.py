bee7ff4d5b86e8327a8dce3535dcceb341545074
cecillia8816 <cecillia8816@users.noreply.github.com>
2026-09-16 09:42:39 +0800

    dashboards 2026-09-15

---
+++ _runpy.py
@@ -0,0 +1,4 @@
+﻿import subprocess, os
+os.chdir(r'E:\Cecillia2011-04\总经办助理 20190819\营运部\门店报表\.workbuddy\github_pages\bake-ops')
+p = subprocess.run(['git', 'ls-remote', 'origin', 'HEAD'], capture_output=True, text=True, timeout=90)
+open(r'E:\Cecillia2011-04\总经办助理 20190819\营运部\门店报表\.workbuddy\github_pages\bake-ops\_push_log.txt', 'w', encoding='utf-8').write(f'exit={p.returncode}\nSTDOUT:{p.stdout}\nSTDERR:{p.stderr}')
